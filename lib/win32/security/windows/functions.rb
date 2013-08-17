b2f21594cce275ab9c5c50270b0a6c8d98356ba9
Eric Badger <badgerea@hotmail.com>
2013-08-17 09:58:03 -0500

    add some ACL related functions

--- lib/win32/security/windows/functions.rb
+++ lib/win32/security/windows/functions.rb
@@ -72,6 +72,8 @@ module Windows
       ffi_lib :advapi32
 
       attach_pfunc :AddAccessAllowedAce, [:ptr, :dword, :dword, :ptr], :bool
+      attach_pfunc :AddAce, [:ptr, :dword, :dword, :ptr, :dword], :bool
+      attach_pfunc :AdjustTokenPrivileges, [:handle, :bool, :ptr, :dword, :ptr, :ptr], :bool
       attach_pfunc :AllocateAndInitializeSid,
         [:ptr, :int, :dword, :dword, :dword, :dword, :dword, :dword, :dword, :dword, :ptr], :bool
       attach_pfunc :CheckTokenMembership, [:handle, :ptr, :ptr], :bool
@@ -79,12 +81,17 @@ module Windows
       attach_pfunc :ConvertStringSidToSid, :ConvertStringSidToSidA, [:string, :ptr], :bool
       attach_pfunc :EqualSid, [:ptr, :ptr], :bool
       attach_pfunc :FindFirstFreeAce, [:ptr, :ptr], :bool
+      attach_pfunc :GetAce, [:ptr, :dword, :pointer], :bool
       attach_pfunc :GetAclInformation, [:ptr, :ptr, :dword, :int], :bool
       attach_pfunc :GetLengthSid, [:ptr], :dword
+      attach_pfunc :GetNamedSecurityInfo, :GetNamedSecurityInfoA, [:ptr, :int, :int, :ptr, :ptr, :ptr, :ptr, :ptr], :int
+      attach_pfunc :GetSecurityDescriptorControl, [:ptr, :ptr, :ptr], :bool
       attach_pfunc :GetSidLengthRequired, [:uint], :dword
       attach_pfunc :GetSidSubAuthority, [:ptr, :dword], :ptr
       attach_pfunc :GetTokenInformation, [:handle, :token_info_class, :ptr, :dword, :ptr], :bool
+      attach_pfunc :GetUserName, :GetUserNameA, [:ptr, :ptr], :bool
       attach_pfunc :InitializeAcl, [:ptr, :dword, :dword], :bool
+      attach_pfunc :InitializeSecurityDescriptor, [:ptr, :dword], :bool
       attach_pfunc :InitializeSid, [:ptr, :ptr, :uint], :bool
       attach_pfunc :IsValidAcl, [:ptr], :bool
       attach_pfunc :IsValidSid, [:ptr], :bool
@@ -93,9 +100,15 @@ module Windows
         [:string, :string, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
       attach_pfunc :LookupAccountSid, :LookupAccountSidA,
         [:string, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
+      attach_pfunc :LookupPrivilegeName, :LookupPrivilegeNameA, [:ptr, :ptr, :ptr, :ptr], :bool
+      attach_pfunc :LookupPrivilegeValue, :LookupPrivilegeValueA, [:ptr, :ptr, :ptr], :bool
       attach_pfunc :OpenProcessToken, [:handle, :dword, :ptr], :bool
       attach_pfunc :OpenThreadToken, [:handle, :dword, :bool, :ptr], :bool
       attach_pfunc :SetAclInformation, [:ptr, :ptr, :dword, :int], :bool
+      attach_pfunc :SetNamedSecurityInfo, :SetNamedSecurityInfoA, [:ptr, :int, :int, :ptr, :ptr, :ptr, :ptr], :ulong
+      # TODO: typdef WORD SECURITY_DESCRIPTOR_CONTROL
+      attach_pfunc :SetSecurityDescriptorControl, [:ptr, :ushort, :ushort], :bool
+      attach_pfunc :SetSecurityDescriptorDacl, [:ptr, :bool, :ptr, :bool], :bool
     end
   end
 end
